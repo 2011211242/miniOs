b02ab54b8603d6009e68fc1381bb2deb72e8de97
2011211242 <2990936748@qq.com>
2019-06-23 17:32:55 +0800

    已经能够成功加载loader，并可以实现跳转

--- boot/boot.asm
+++ boot/boot.asm
@@ -17,170 +17,167 @@ nop
 
 ;stack resb 32
 ;打印一个字符
-DispChar:
-    push    ebp
-    mov     ebp, esp
+;DispChar:
+;    push    ebp
+;    mov     ebp, esp
+;
+;    push    ax
+;    push    bx
+;    push    dx
+;    push    cx
+;
+;    mov	ax, [ebp + 6]
+;    cmp al, 0ah 
+;    jz .DispChar_handle_Ret
+;
+;    mov	dx, 0B800h
+;    mov	gs, dx
+;    mov	ah, 0Fh				; 0000: 黑底    1111: 白字
+;    mov bx, [cursor]
+;
+;    cmp bx, 160 * 25
+;    jnae .DispChar_handle_cursor
+;    mov bx, 0
+;.DispChar_handle_cursor:
+;    mov	[gs:bx], ax	        ;屏幕第 0 行, 第 39 列。
+;    add bx, 02h
+;    mov [cursor], bx
+;    jmp .DispChar_handle_restore
+;
+;.DispChar_handle_Ret:
+;    call  DispRet
+;
+;.DispChar_handle_restore:
+;    pop cx
+;    pop dx
+;    pop bx
+;    pop ax
+;    pop ebp
+;    ret
+;; end of 打印一个字符
 
-    push    ax
-    push    bx
-    push    dx
-    push    cx
-
-    mov	ax, [ebp + 6]
-    cmp al, 0ah 
-    jz .DispChar_handle_Ret
-
-    mov	dx, 0B800h
-    mov	gs, dx
-    mov	ah, 0Fh				; 0000: 黑底    1111: 白字
-    mov bx, [cursor]
-
-    cmp bx, 160 * 25
-    jnae .DispChar_handle_cursor
-    mov bx, 0
-.DispChar_handle_cursor:
-    mov	[gs:bx], ax	        ;屏幕第 0 行, 第 39 列。
-    add bx, 02h
-    mov [cursor], bx
-    jmp .DispChar_handle_restore
-
-.DispChar_handle_Ret:
-    call  DispRet
-
-.DispChar_handle_restore:
-    pop cx
-    pop dx
-    pop bx
-    pop ax
-    pop ebp
-    ret
-; end of 打印一个字符
-
-DispRet:       ;换行
-    push    ebp
-    mov     ebp, esp
-
-    push ax
-    push bx
-
-    mov ax, [cursor]
-    mov bl, 0a0h
-    div bl
-    inc al
-    cmp al, 25
-    jnae .DispRet_handle_cursor
-    mov al, 0
-
-.DispRet_handle_cursor:
-    mul bl
-    mov [cursor], ax
-
-    pop bx
-    pop ax
-    pop ebp
-    ret
+;DispRet:       ;换行
+;    push    ebp
+;    mov     ebp, esp
+;
+;    push ax
+;    push bx
+;
+;    mov ax, [cursor]
+;    mov bl, 0a0h
+;    div bl
+;    inc al
+;    cmp al, 25
+;    jnae .DispRet_handle_cursor
+;    mov al, 0
+;
+;.DispRet_handle_cursor:
+;    mul bl
+;    mov [cursor], ax
+;
+;    pop bx
+;    pop ax
+;    pop ebp
+;    ret
 
 ;打印Word的十六进制数字串
-DispW:
-    push    ebp
-    mov     ebp, esp
-    push    ax
-    push    cx
+;DispW:
+;    push    ebp
+;    mov     ebp, esp
+;    push    ax
+;    push    cx
+;
+;    mov     cl, 010h
+;.DispW_loop:
+;    mov     ax, [ebp + 6]
+;    cmp     cl, 0
+;    jz      .DispW_handle_end
+;    sub     cl, 4
+;    shr     ax, cl
+;    and     ax, 00fh
+;
+;    cmp     al, 09h
+;    ja      .DispW_loop_Digit_handle_greaer_9
+;
+;    add     al, '0'
+;    jmp     .DispW_loop_Digit_handle_print
+;
+;.DispW_loop_Digit_handle_greaer_9:
+;    sub     al, 0ah
+;    add     al, 'a'
+;
+;.DispW_loop_Digit_handle_print:
+;    push    ax
+;    call    DispChar
+;    pop     ax
+;    jmp     .DispW_loop
+;
+;.DispW_handle_end:
+;    pop     cx
+;    pop     ax
+;    pop     ebp
+;    ret
+;;end of 打印dw的十六进制数字串
 
-    mov     cl, 010h
-.DispW_loop:
-    mov     ax, [ebp + 6]
-    cmp     cl, 0
-    jz      .DispW_handle_end
-    sub     cl, 4
-    shr     ax, cl
-    and     ax, 00fh
+;Clear_Screen:
+;    push    ebp
+;    mov     ebp, esp
+;
+;    push    ax
+;    push    cx
+;
+;    mov     ax, ' ' ;空格
+;    sub     esp, 2
+;    mov     [esp], ax
+;
+;    mov     cx, 80 * 25
+;
+;Clear_Screen_loop:          
+;    call    DispChar
+;    loop    Clear_Screen_loop
+;
+;    add     esp, 2
+;
+;    pop     cx
+;    pop     ax
+;    pop     ebp
+;    ret
+;;end of Clear_Screen
 
-    cmp     al, 09h
-    ja      .DispW_loop_Digit_handle_greaer_9
-
-    add     al, '0'
-    jmp     .DispW_loop_Digit_handle_print
-
-.DispW_loop_Digit_handle_greaer_9:
-    sub     al, 0ah
-    add     al, 'a'
-
-.DispW_loop_Digit_handle_print:
-    push    ax
-    call    DispChar
-    pop     ax
-    jmp     .DispW_loop
-
-.DispW_handle_end:
-    pop     cx
-    pop     ax
-    pop     ebp
-    ret
-;end of 打印dw的十六进制数字串
-
-Clear_Screen:
-    push    ebp
-    mov     ebp, esp
-
-    push    ax
-    push    cx
-
-    mov     ax, ' ' ;空格
-    sub     esp, 2
-    mov     [esp], ax
-
-    mov     cx, 80 * 25
-
-Clear_Screen_loop:          
-    call    DispChar
-    loop    Clear_Screen_loop
-
-    add     esp, 2
-
-    pop     cx
-    pop     ax
-    pop     ebp
-    ret
-;end of Clear_Screen
-
-DispStr:
-    push    ebp
-    mov     ebp, esp
-
-    push    si
-    push    cx
-    push    bx
-    push    es
-
-    mov     cx, [ebp + 10] 
-    mov     es, [ebp + 8]
-    mov     si, [ebp + 6]
-
-    xor     bh, bh
-.DispStr_loop:
-    mov     bl, [es:si]
-    push    bx
-    call    DispChar
-    pop     bx
-    inc     si
-    loop    .DispStr_loop
-
-    pop     es
-    pop     bx
-    pop     cx
-    pop     si 
-    pop     ebp 
-    ret
-; end of DispStr
-
-
-ResetDisk:
-    xor     ah, ah
-    xor     dl, dl
-    int     13h
-    ret
-
+;DispStr:
+;    push    ebp
+;    mov     ebp, esp
+;
+;    push    si
+;    push    cx
+;    push    bx
+;    push    es
+;
+;    mov     cx, [ebp + 10] 
+;    mov     es, [ebp + 8]
+;    mov     si, [ebp + 6]
+;
+;    xor     bh, bh
+;.DispStr_loop:
+;    mov     bl, [es:si]
+;    push    bx
+;    call    DispChar
+;    pop     bx
+;    inc     si
+;    loop    .DispStr_loop
+;
+;    pop     es
+;    pop     bx
+;    pop     cx
+;    pop     si 
+;    pop     ebp 
+;    ret
+;; end of DispStr
+;ResetDisk:
+;    xor     ah, ah
+;    xor     dl, dl
+;    int     13h
+;    ret
 ;------------------------------------
 ;dw      起始扇区号
 ;dw      要读的扇区数, <= 256
@@ -244,53 +241,74 @@ ReadDir:
 GetFATEntry:
     push    ebp
     mov     ebp, esp
-    
-
-    pop     ebp
-    ret
-
-DispBootMessage:
-    push    ebp
-    mov     ebp, esp
     push    ax
+    push    bx
+    push    dx
+    push    es
 
-    sub     esp, 6 
-    mov     ax, [boot_message_len] 
-    mov     [esp + 4], ax
-    mov     [esp + 2], ds
-    mov     ax, boot_message 
-    mov     [esp], ax
+    mov     ax, [ebp + 6]
+    mov     [IsOdd], byte 0
+    mov     bx, 3
+    mul     bx
+    mov     bx, 2
+    mov     dx, 0               ;被除数32位,高位存在dx中
+    div     bx
+    mov     byte [IsOdd], dl    ;余数是否为0
+                                ;ax为FATEntry 在FAT中的偏移量
+    mov     dx,  0
+    mov     bx, [BPB_BytsPerSec]
+    div     bx                  
+    add     ax, SectorNoOfFAT1  ;ax为扇区号
+                                ;dx存储偏移
 
-    call    DispStr
-    add     esp, 6
-    call    DispRet
+    sub     esp, 8
+    mov     [esp + 6], ax
+    mov     [esp + 4], word 2
+    mov     [esp + 2], word BaseOfFAT
+    mov     [esp],     word OffsetOfFAT
+    call    ReadSec
+    add     esp, 8
     
+    mov     ax, word BaseOfFAT
+    mov     es, ax
+    mov     bx, dx
+    mov     ax, [es:bx]     ;ax为对应数据的扇区号
+
+    cmp     byte [IsOdd], 1
+    jnz     .FATEntry_even
+    shr     ax, 4
+.FATEntry_even:
+    and     ax, 0FFFh
+    mov     [ebp + 8], ax
+
+    pop     es
+    pop     dx
+    pop     bx
     pop     ax
     pop     ebp
     ret
+
+;DispBootMessage:
+;    push    ebp
+;    mov     ebp, esp
+;    push    ax
+;
+;    sub     esp, 6 
+;    mov     ax, [boot_message_len] 
+;    mov     [esp + 4], ax
+;    mov     [esp + 2], ds
+;    mov     ax, boot_message 
+;    mov     [esp], ax
+;
+;    call    DispStr
+;    add     esp, 6
+;    call    DispRet
+;    
+;    pop     ax
+;    pop     ebp
+;    ret
 ;end of DispDebugMessage
 
-
-DispDebugMessage:
-    push    ebp
-    mov     ebp, esp
-    push    ax
-
-    sub     esp, 6 
-    mov     ax, [debug_message_len] 
-    mov     [esp + 4], ax
-    mov     [esp + 2], ds
-    mov     ax, debug_message 
-    mov     [esp], ax
-
-    call    DispStr
-    add     esp, 6
-    call    DispRet
-    
-    pop     ax
-    pop     ebp
-    ret
-;end of DispDebugMessage
 
 ; 比较字符串是否相等
 ; ret       +16 +10   0 equal 1 not equal
@@ -354,7 +372,6 @@ FindLoader:
     push    di
 
     mov     cx, [BPB_RootEntCnt]
-
     sub     esp, 12
     mov     [esp + 8], word BaseOfLoader
     mov     [esp + 6], word OffsetOfLoader
@@ -386,15 +403,52 @@ FindLoader:
     ret
 ;end of FindLoader
 
-cursor                  dw 160 * 0
-boot_message            dw "boot ..."
-boot_message_len        dw $ - boot_message
+LoadLoader:
+    push    ebp
+    mov     ebp, esp
+    push    ax
+    push    bx 
 
-debug_message           dw "loader founded..."
-debug_message_len       dw $ - debug_message
+    mov     ax, [Loader_DIR_FstClus]
+    mov     bx, word BaseOfLoader
+.loop_LoadLoader:
+    push    ax
+    add     ax, SectorNoOfData
+    sub     esp, 8
+    mov     [esp + 6], ax
+    mov     [esp + 4], word 001h
+    mov     [esp + 2], bx
+    mov     [esp], word OffsetOfLoader      
+    call    ReadSec
+    add     esp, 8
+
+    pop     ax
+    sub     esp, 4
+    mov     [esp], ax
+    call    GetFATEntry
+    mov     ax, [esp + 2]
+    add     esp, 4
+
+    add     bx, 20h
+    cmp     ax, 0fffh
+    jnz     .loop_LoadLoader
+
+    pop     bx
+    pop     ax
+    pop     ebp
+    ret
+
+;end of LoadLoader
+;cursor                  dw 160 * 0
+;boot_message            dw "boot ..."
+;boot_message_len        dw $ - boot_message
+
+;debug_message           dw "loader founded..."
+;debug_message_len       dw $ - debug_message
 
 LoaderFileName          db  "LOADER  BIN", 0 ;LOADER.COM文件名
 Loader_DIR_FstClus      dw  0
+IsOdd                   db 0
 
 
 start:
@@ -405,20 +459,11 @@ start:
     mov esp, BaseOfStack 
     mov ebp, esp
 
-    call    Clear_Screen
-    call    DispBootMessage
     call    ReadDir
     call    FindLoader
-    push    word [Loader_DIR_FstClus]
-    call    DispW
-    pop     ax
-
-
+    call    LoadLoader
 end:
-    jmp $
-    mov ax, 4c00h
-    int 21h
-    jmp $
+    jmp BaseOfLoader:OffsetOfLoader
 
 ;times 	2510-($-$$)	db	0	; 填充剩下的空间，使生成的二进制代码恰好为512字节
 ;dw 	0xaa55				; 结束标志
--- boot/loader.asm
+++ boot/loader.asm
@@ -1,10 +1,16 @@
 org	0100h
+jmp start
+times 	1024-($-$$)	db	0	; 填充剩下的空间，使生成的二进制代码恰好为512字节
 
+msg db "this is loader"
+
+start                   ; Start
 	mov	ax, 0B800h
 	mov	gs, ax
 	mov	ah, 0Fh				; 0000: 黑底    1111: 白字
 	mov	al, 'L'
 	mov	[gs:((80 * 0 + 39) * 2)], ax	; 屏幕第 0 行, 第 39 列。
 
-	jmp	$		; Start
+	jmp	$		
 
+;dw 	    0xaa55				; 结束标志
