2bc4882562327e9624bb9760468baa86e94fd726
2011211242 <2990936748@qq.com>
2019-06-24 08:41:04 +0800

    测试大loader

--- boot/loader.asm
+++ boot/loader.asm
@@ -1,10 +1,11 @@
 org	0100h
+jmp start
 
+times 	23510-($-$$)	db	0	; 填充剩下的空间，使生成的二进制代码恰好为512字节
 start:
 	mov	ax, 0B800h
 	mov	gs, ax
 	mov	ah, 0Fh				; 0000: 黑底    1111: 白字
-	mov	al, 'L'
-	mov	[gs:((80 * 0 + 39) * 2)], ax	; 屏幕第 0 行, 第 39 列。
-
+	mov	al, 'A'
+	mov	[gs:((80 * 0 + 0) * 2)], ax	; 屏幕第 0 行, 第 39 列。
 	jmp	$		
