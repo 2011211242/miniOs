f56a093a456c2dc41ac7e734e581fd2dfe52d8c5
2011211242 <2990936748@qq.com>
2019-06-29 17:01:57 +0800

    modify Makefile to generate code easily

--- kernel/kernel.asm
+++ kernel/kernel.asm
@@ -1,1 +1,23 @@
-jmp $
+
+;[section .bss]
+;KernelMessage             db  "Kernel started ...", 0AH,0DH
+;KernelMessageLen          equ  $-KernelMessage             
+
+
+[section .text]
+global _start
+
+_start: 
+    mov     ax, cs
+    mov     ds, ax
+
+    ;mov     cx, KernelMessageLen
+    ;mov     bp, word KernelMessage
+    mov     ax, 01301h
+    mov     bx, 000fh
+    mov     dx, cs
+    mov     es, dx
+    mov     dx, 000h
+    int     10h
+
+	jmp	$		
