0b58820af823c32ea3859829881f4d60bc2775a4
2011211242 <2990936748@qq.com>
2019-07-08 22:42:31 +0800

    add *

--- boot/loader.asm
+++ boot/loader.asm
@@ -3,9 +3,9 @@ jmp start
 
 %define LOADER_BIN
 %include "loader.inc"
-%include "pm.inc"
 %include "lib.inc"
 %include "fat12hdr.inc"
+%include "pm.inc"
 
 ;                                   段基址                  段界限                              属性
 LABEL_GDT:              Descriptor  0,                      0,                                  0       ;空描述符   
@@ -128,7 +128,8 @@ start:
     mov     ds, ax
     mov     es, ax
     mov     ss, ax
-    mov     esp, OffsetOfLoader      
+    mov     esp, TopOfStack
+
 
 ReadDir:
     sub     esp, 8
@@ -233,17 +234,38 @@ LoadLoader:
     jnz     .loop_LoadLoader
 
 
-    push    ax
-    push    word 20h
-    push    BaseOfKernel
-    push    OffsetOfKernel
-    call    DispStr
-    pop     ax
-    pop     ax
-    pop     ax
-    pop     ax
+    ;push    ax         ;调试信息
+    ;push    word 20h
+    ;push    BaseOfKernel
+    ;push    OffsetOfKernel
+    ;call    DispStr
+    ;pop     ax
+    ;pop     ax
+    ;pop     ax
+    ;pop     ax
 
 
+GetMemInfo:
+    mov     ax, cs
+    mov     es, ax
+
+    mov     ebx, 0
+    mov     di, MemChkBuf
+.MemChkLoop:
+    mov     eax, 0E820h
+    mov     ecx, 20
+    mov     edx, 0534D4150h
+    int     15h
+    jc      .MemChkFail
+    add     di, 20
+    inc     dword [dwMCRNumber]
+    cmp     ebx, 0
+    jne     .MemChkLoop
+    jmp     .MemChkOK
+.MemChkFail:
+    mov     dword [dwMCRNumber], 0 
+.MemChkOK:
+    
 GO_TO_PM_MODE:
     xor     eax, eax                            ;初始化栈区
     mov     ax,  cs
@@ -265,6 +287,8 @@ GO_TO_PM_MODE:
 	mov	eax, cr0            ; 准备切换到保护模式
 	or	eax, 1
 	mov	cr0, eax
+    mov eax, [cursor]
+    mov [pm_disp_pos], eax
 	jmp	dword SelectorFlatC:(BaseOfLoaderPhyAddr+LABEL_PM_START)    ; 真正进入保护模式
 
 
@@ -272,6 +296,167 @@ GO_TO_PM_MODE:
 ALIGN   32      ;变量的对齐方式
 [BITS   32]     ;选择32位指令
 %include "pmlib.inc"
+
+DispMemInfo:
+    push    esi
+    push    edi
+    push    ecx
+
+    push    30h
+    push    szMemChkTitle
+    call    PM_DispStr
+    add     esp, 8
+
+    mov     esi, MemChkBuf
+    mov     ecx, [dwMCRNumber]
+.loop:
+    mov     edx, 5
+    mov     edi, ARDStruct
+.loop1:
+    push    dword [esi]
+    call    PM_DispDW
+    pop     eax
+    stosd
+    add     esi, 4
+    dec     edx
+    cmp     edx, 0
+    jnz     .loop1
+    call    PM_DispRet
+    cmp     dword [dwType], 1
+    jne     .2
+    mov     eax, [dwBaseAddrLow]
+    add     eax, [dwLengthLow]
+    cmp     eax, [dwMemSize]
+    jb      .2
+    mov     [dwMemSize], eax
+.2:
+    loop    .loop
+    push    dword 0009h
+    push    dword szRAMSize
+    call    PM_DispStr
+    add     esp, 8
+
+    push    dword [dwMemSize]
+    call    PM_DispDW
+    add     esp, 4
+    pop     ecx
+    pop     edi
+    pop     esi
+    ret
+
+MemCpy:
+    push    ebp
+    mov ebp, esp
+
+    push    esi
+    push    edi
+    push    ecx
+
+    mov edi, [ebp + 8]  ; Destination
+    mov esi, [ebp + 12] ; Source
+    mov ecx, [ebp + 16] ; Counter
+.mem_cpy_1:
+    cmp ecx, 0      ; 判断计数器
+    jz  .mem_cpy_2  ; 计数器为零时跳出
+
+    mov al, [ds:esi]        ; ┓
+    inc esi         ; ┃
+                    ; ┣ 逐字节移动
+    mov byte [es:edi], al   ; ┃ 
+    inc edi         ; ┛
+
+    dec ecx         ; 计数器减一
+    jmp .mem_cpy_1  ; 循环
+.mem_cpy_2:
+    ;mov eax, [ebp + 8]  ; 返回值
+    pop ecx 
+    pop edi 
+    pop esi 
+    mov esp, ebp 
+    pop ebp 
+    ret
+
+; 启动分页机制 --------------------------------------------------------------
+SetupPaging:
+	; 根据内存大小计算应初始化多少PDE以及多少页表
+	xor	edx, edx
+	mov	eax, [dwMemSize]
+	mov	ebx, 400000h	; 400000h = 4M = 4096 * 1024, 一个页表对应的内存大小
+	div	ebx
+	mov	ecx, eax	; 此时 ecx 为页表的个数，也即 PDE 应该的个数
+	test	edx, edx
+	jz	.no_remainder
+	inc	ecx		; 如果余数不为 0 就需增加一个页表
+.no_remainder:
+	push	ecx		; 暂存页表个数
+
+	; 为简化处理, 所有线性地址对应相等的物理地址. 并且不考虑内存空洞.
+
+	; 首先初始化页目录
+	mov	ax, SelectorFlatRW
+	mov	es, ax
+	mov	edi, PageDirBase	; 此段首地址为 PageDirBase
+	xor	eax, eax
+	mov	eax, PageTblBase | PG_P  | PG_USU | PG_RWW
+.1:
+	stosd
+	add	eax, 4096		; 为了简化, 所有页表在内存中是连续的.
+	loop	.1
+
+	; 再初始化所有页表
+	pop	eax			; 页表个数
+	mov	ebx, 1024		; 每个页表 1024 个 PTE
+	mul	ebx
+	mov	ecx, eax		; PTE个数 = 页表个数 * 1024
+	mov	edi, PageTblBase	; 此段首地址为 PageTblBase
+	xor	eax, eax
+	mov	eax, PG_P  | PG_USU | PG_RWW
+.2:
+	stosd
+	add	eax, 4096		; 每一页指向 4K 的空间
+	loop	.2
+
+	mov	eax, PageDirBase
+	mov	cr3, eax
+	mov	eax, cr0
+	or	eax, 80000000h
+	mov	cr0, eax
+	jmp	short .3
+.3:
+	nop
+
+	ret
+; 分页机制启动完毕 ----------------------------------------------------------
+
+
+
+; InitKernel ---------------------------------------------------------------------------------
+; 将 KERNEL.BIN 的内容经过整理对齐后放到新的位置
+; --------------------------------------------------------------------------------------------
+InitKernel:	; 遍历每一个 Program Header，根据 Program Header 中的信息来确定把什么放进内存，放到什么位置，以及放多少。
+	xor	esi, esi
+	mov	cx, word [BaseOfKernelFilePhyAddr + 2Ch]; ┓ ecx <- pELFHdr->e_phnum
+	movzx	ecx, cx					; ┛
+	mov	esi, [BaseOfKernelFilePhyAddr + 1Ch]	; esi <- pELFHdr->e_phoff
+	add	esi, BaseOfKernelFilePhyAddr		; esi <- OffsetOfKernel + pELFHdr->e_phoff
+.Begin:
+	mov	eax, [esi + 0]
+	cmp	eax, 0				; PT_NULL
+	jz	.NoAction
+	push	dword [esi + 010h]		; size	┓
+	mov	eax, [esi + 04h]		;	┃
+	add	eax, BaseOfKernelFilePhyAddr	;	┣ ::memcpy(	(void*)(pPHdr->p_vaddr),
+	push	eax				; src	┃		uchCode + pPHdr->p_offset,
+	push	dword [esi + 08h]		; dst	┃		pPHdr->p_filesz;
+	call	MemCpy				;	┃
+	add	esp, 12				;	┛
+.NoAction:
+	add	esi, 020h			; esi += pELFHdr->e_phentsize
+	dec	ecx
+	jnz	.Begin
+	ret
+; InitKernel ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^
+
 
 LABEL_PM_START:
     mov     ax, SelectorVideo
@@ -289,25 +474,10 @@ LABEL_PM_START:
     push    BootMessageLen
     push    BootMessage
     call    PM_DispStr
-    call    PM_DispStr
-    call    PM_DispStr
-    call    PM_DispStr
+    add     esp, 8
 
-    ;push    word 'P'
-    ;call    PM_DispChar
-    ;jmp     $
+    call    DispMemInfo
 
-    push    esp
-    call    PM_DispRet
-
-    push    word ' '
-    push    esp
-    call    PM_DispDW
-    jmp     $
-
-    ;ret
-    ;call    LoadLoader
-    ;jmp BaseOfLoader:OffsetOfLoader
 
 
 [SECTION .data]
@@ -317,6 +487,7 @@ BootMessageLen          equ  $ - BootMessage
 
 StackSize               equ     1024
 StackSpace:             times   StackSize   db  0
+TopOfStack              equ $
 
 KernelFileName          db  "KERNEL  BIN", 0 ;LOADER.COM文件名
 Loader_DIR_FstClus      dw  0
@@ -328,3 +499,20 @@ NoKernelMessageLen      equ $ - NoKernelMessage
 KernelFindMessage       db "Kernel find", 0AH
 KernelFindMessageLen    equ $ - KernelFindMessage       
 
+LABEL_DATA:
+; 保护模式下使用这些符号
+; 字符串
+szMemChkTitle:         db  "BaseAddrL BaseAddrH LengthLow LengthHigh   Type", 0Ah, 0
+szRAMSize:         db  "RAM size:", 0
+szReturn:          db  0Ah, 0
+;; 变量
+dwMCRNumber:       dd   0; Memory Check Result
+dwDispPos:         dd  (80 * 6 + 0) * 2    ; 屏幕第 6 行, 第 0 列。
+dwMemSize:         dd  0
+ARDStruct:         ; Address Range Descriptor Structure
+    dwBaseAddrLow:     dd  0
+    dwBaseAddrHigh:    dd  0
+    dwLengthLow:       dd  0
+    dwLengthHigh:      dd  0
+    dwType:        dd  0
+MemChkBuf: times   256 db  0
