48ac2b8b2ee2ce91e2e6bffafaf121df6629ff98
2011211242 <2990936748@qq.com>
2019-07-31 08:48:30 +0800

    add init 8295A

--- kernel/include/protect.h
+++ kernel/include/protect.h
@@ -89,4 +89,10 @@ typedef struct s_gate
 #define	INT_VECTOR_IRQ0			0x20
 #define	INT_VECTOR_IRQ8			0x28
 
+/* 8259A interrupt controller ports. */
+#define INT_M_CTL     0x20 /* I/O port for interrupt controller       <Master> */
+#define INT_M_CTLMASK 0x21 /* setting bits in this port disables ints <Master> */
+#define INT_S_CTL     0xA0 /* I/O port for second interrupt controller<Slave>  */
+#define INT_S_CTLMASK 0xA1 /* setting bits in this port disables ints <Slave>  */
+
 #endif
--- kernel/src/start.c
+++ kernel/src/start.c
@@ -30,7 +30,7 @@ void gdt_init() {
     asm ("lgdt (,%0,1)"::"r"(gdt_ptr):);
 }
 
-void out_type(short port, char value)
+static void out_byte(short port, char value)
 {
     asm("mov %0, %%dx; \
             mov %1, %%al; \
@@ -42,12 +42,52 @@ void out_type(short port, char value)
     );
 }
 
-void in_byte(short port)
+static char in_byte(short port)
 {
-
+    char ret;
+    asm("mov %1, %%dx; \
+            xor %%al, %%al; \
+            in %%dx, %%al; \
+            mov %%al, %0; \
+            nop; nop;" 
+            :"=r"(ret)
+            :"r"(port)
+            :"%al", "%dx"
+    );
+    return ret;
 }
 
 void init_8295A()
 {
+    /* Master 8259, ICW1. */
+    out_byte(INT_M_CTL, 0x11);
 
+    /* Slave  8259, ICW1. */
+    out_byte(INT_S_CTL, 0x11);
+
+    /* Master 8259, ICW2. 设置 '主8259' 的中断入口地址为 0x20. */
+    out_byte(INT_M_CTLMASK, INT_VECTOR_IRQ0);
+
+    /* Slave  8259, ICW2. 设置 '从8259' 的中断入口地址为 0x28 */
+    out_byte(INT_S_CTLMASK, INT_VECTOR_IRQ8);
+
+    /* Master 8259, ICW3. IR2 对应 '从8259'. */
+    out_byte(INT_M_CTLMASK, 0x4);
+
+    /* Slave  8259, ICW3. 对应 '主8259' 的 IR2. */
+    out_byte(INT_S_CTLMASK, 0x2);
+
+    /* Master 8259, ICW4. */
+    out_byte(INT_M_CTLMASK, 0x1);
+
+    /* Slave  8259, ICW4. */
+    out_byte(INT_S_CTLMASK, 0x1);
+
+    /* Master 8259, OCW1.  */
+    out_byte(INT_M_CTLMASK, 0xFD);
+
+    /* Slave  8259, OCW1.  */
+    out_byte(INT_S_CTLMASK, 0xFF);
 }
+
+
